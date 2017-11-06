89e63250ea149599d9eaf9bf7e9384ed5b2d6093
Drew Demechko <drew_demechko@yahoo.com>
2017-11-05 20:39:09 -0500

    Created controllers for adding/deleting books to and from user libraries/wishlists.

---
+++ MyReadingTrackerAPI/Controllers/BookLibrariesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyReadingTrackerAPI.Controllers
+{
+    [Route("api/Libraries")]
+    public class BookLibrariesController : Controller
+    {
+        //Add to users library (a stored procedure, should this be a post?)
+    }
+}
---
+++ MyReadingTrackerAPI/Controllers/BookWishListsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyReadingTrackerAPI.Controllers
+{
+    [Route("api/WishLists")]
+    public class BookWishListsController : Controller
+    {
+        //Add to users wish list (a stored procedure, should this be a post?)
+    }
+}
